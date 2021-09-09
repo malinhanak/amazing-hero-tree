9c2525da0f1381748d852dc1a57fcd5a803f61ea
Malin Hanak <malinehanak@gmail.com>
2021-09-09 11:43:27 +0200

    theme files

---
+++ src/theme/themeDC.js
@@ -0,0 +1,16 @@
+import DCbg from "../assets/quinn.jpeg";
+
+export const themeDC = {
+    images: {
+        background: DCbg,
+    },
+    colors: {
+        body: "1, 38, 89",
+        primary: "255, 255, 255",
+        secondary: "",
+        font: "",
+    },
+    toggler: {
+        font: "black",
+    },
+};
---
+++ src/theme/themeMarvel.js
@@ -0,0 +1,16 @@
+import Marvelbg from "../assets/marvel.jpeg";
+
+export const themeMarvel = {
+    images: {
+        background: Marvelbg,
+    },
+    colors: {
+        body: "1, 38, 89",
+        primary: "255, 255, 255",
+        secondary: "",
+        font: "",
+    },
+    toggler: {
+        font: "black",
+    },
+};
