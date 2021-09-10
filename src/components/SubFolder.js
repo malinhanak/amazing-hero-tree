15face0c02b3a49e2699f2a71a69eff2dac69b2a
Malin Hanak <malinehanak@gmail.com>
2021-09-10 08:59:07 +0200

    Subfolder with comments

--- src/components/SubFolder.js
+++ src/components/SubFolder.js
@@ -3,9 +3,21 @@ import Accordion from "./accordion/Accordion";
 import AccordionTitle from "./accordion/AccordionTitle";
 import File from "./File";
 
+/*
+    SubFolder get 4 props:
+    - data: some object or array of object data
+    - isOpen: state check from parent to determend of a folder is in fact open.
+    - Action to potentially open or close given folder.
+    - the current state of the app.
+*/
+
 function SubFolder({ data, isOpen, action, state }) {
+    /*
+    maps over the data given to it via FileTree, in this case, subfolder object 
+    and file arrays.
+    */
     return data.map((sublevel) => {
-        const subFolderName = sublevel.folder;
+        const subFolderName = sublevel.folder; // variable to hold the current folder, used for id and state check.
         return (
             isOpen && (
                 <Accordion key={subFolderName} isSub={true}>
