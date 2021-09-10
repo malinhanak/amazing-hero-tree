339fe0506f3ff593c9ad5cd67f34cb647c87df90
Malin Hanak <malinehanak@gmail.com>
2021-09-10 08:44:31 +0200

    root

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { ThemeProvider } from "./context/ThemeContext";
+import reportWebVitals from "./reportWebVitals";
+import { GlobalStyle } from "./theme/GlobalStyle";
+import "@fontsource/open-sans-condensed";
+import "@fontsource/open-sans";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ThemeProvider>
+            <GlobalStyle />
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
