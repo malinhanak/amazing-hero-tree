61d42f6aec817e048cc20497778f44239c67c0bb
Malin Hanak <malinehanak@gmail.com>
2021-09-10 08:35:11 +0200

    New App structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import FileTree from "./components/FileTree";
+import Footer from "./components/Footer";
+import FileList from "./file.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Container>
+                <FileTree tree={FileList} />
+            </Container>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
+
+const Container = styled.section`
+    background: rgba(${(props) => props.theme.colors.primary}, 0.8);
+    padding: 1.5rem;
+    border-radius: 20px 5px 20px 5px;
+    max-width: 550px;
+    width: 90%;
+    margin: 2rem auto;
+`;
